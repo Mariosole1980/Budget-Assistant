d9fe919d45fe2b5a23c2dbd02252805da128f079
Mariosole1980 <marios.ko@hotmail.com>
2026-07-31 21:40:12 +0300

    release: build v960 [automated release workflow]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW Version 959
+// SW Version 960
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v959-' + Date.now();
+const CACHE_NAME = 'money-manager-v960-' + Date.now();
 const ASSETS = [
   'index.html',
   'style.css',
