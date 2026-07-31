1e6b486f78e5cfcbe6aeeae8446759ad6cacb29a
Mariosole1980 <marios.ko@hotmail.com>
2026-08-01 01:36:17 +0300

    release: build v979 [automated release workflow]

--- www/sw.js
+++ www/sw.js
@@ -1,6 +1,6 @@
-// SW Version 978
+// SW Version 979
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v978-' + Date.now();
+const CACHE_NAME = 'money-manager-v979-' + Date.now();
 const ASSETS = [
   'index.html',
   'style.css',
