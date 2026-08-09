5c654c19ef3d8447106a78a12cc2beabc6b6dd4d
Mariosole1980 <marios.ko@hotmail.com>
2026-08-09 19:05:24 +0300

    release: build v1193 [automated release workflow]

--- www/sw.js
+++ www/sw.js
@@ -1,6 +1,6 @@
-// SW Version 1192
+// SW Version 1193
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v1192-' + Date.now();
+const CACHE_NAME = 'money-manager-v1193-' + Date.now();
 const ASSETS = [
   'index.html',
   'manifest.json',
