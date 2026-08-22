48fbb540bd6ccdd78fa7d05acd08e0bfcb54f6e6
Mariosole1980 <marios.ko@hotmail.com>
2026-08-22 16:49:46 +0300

    release: build v1453 [automated universal release]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW Version 1452
+// SW Version 1453
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v1452-' + Date.now();
+const CACHE_NAME = 'money-manager-v1453-' + Date.now();
 const ASSETS = [
   'index.html',
   'style.css',
