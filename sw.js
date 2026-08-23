81c0ca3ab43b6c0e4de2a58f821f53febac7636c
Mariosole1980 <marios.ko@hotmail.com>
2026-08-24 02:23:18 +0300

    release: build v1528 [automated universal release]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW Version 1527
+// SW Version 1528
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v1527-' + Date.now();
+const CACHE_NAME = 'money-manager-v1528-' + Date.now();
 const ASSETS = [
   'index.html',
   'splash.html',
