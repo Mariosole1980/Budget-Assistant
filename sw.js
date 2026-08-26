3dea7f64cdba4e412165cccf5cc5d51a713a4ce6
Mariosole1980 <marios.ko@hotmail.com>
2026-08-27 01:03:00 +0300

    chore: sync version markers to v1586

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW Version 1585
+// SW Version 1586
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v1585-' + Date.now();
+const CACHE_NAME = 'money-manager-v1586-' + Date.now();
 const ASSETS = [
   'index.html',
   'splash.html',
