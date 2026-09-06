dc1b75a5b6a9315b93bfeeddef3634ceac7a3048
Mariosole1980 <marios.ko@hotmail.com>
2026-09-06 23:51:14 +0300

    release: build v1659 [automated universal release]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW Version 1658
+// SW Version 1659
 const CACHE_VERSION = 'v' + Date.now();
-const CACHE_NAME = 'money-manager-v1658-' + Date.now();
+const CACHE_NAME = 'money-manager-v1659-' + Date.now();
 const ASSETS = [
   'index.html',
   'splash.html',
